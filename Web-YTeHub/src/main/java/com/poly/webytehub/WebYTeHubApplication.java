01722b425991c7c3d044dca0647368b948cac77c
Phong Huynh <phonghuynh2006@gmail.com>
2026-04-10 16:58:53 +0700

    phonghuynh tạo lại dự án

---
+++ Web-YTeHub/src/main/java/com/poly/webytehub/WebYTeHubApplication.java
@@ -0,0 +1,13 @@
+package com.poly.webytehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebYTeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebYTeHubApplication.class, args);
+    }
+
+}
---
+++ Web-YTeHub/src/test/java/com/poly/webytehub/WebYTeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.webytehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebYTeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
